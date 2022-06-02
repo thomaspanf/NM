857031ee657423e1721a249ec0b308b298200b2d
Thomas Pan <thomaspanf@gmail.com>
2022-06-01 19:30:42 -0700

    agent

--- agent/src/agent.spec.ts
+++ agent/src/agent.spec.ts
@@ -6,58 +6,59 @@ import {
   TransactionEvent,
 } from 'forta-agent';
 import {
-  provideCreateAgent, 
+  provideCreateAgent,
 } from './agent';
-import { 
-  encodeFunctionSignature, 
+import {
+  encodeFunctionSignature,
 } from "forta-agent-tools";
 import {
-  TestTransactionEvent, 
-  createAddress
-} from "forta-agent-tools/lib/tests"
+  TestTransactionEvent,
+  createAddress 
+} from "forta-agent-tools/lib/tests" 
 
-const CONTRACT_ADDRESS = createAddress('0x1');
-const ALERT_ID = 'Kill me Agent test';
-const SENDER_ADDRESS = createAddress('0x5');
-
+// const FORTA_ADDRESS = createAddress('0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8');
+const FORTA_ADDRESS = createAddress('0x1'); 
+const ALERT_ID = 'TEST ALERT';
+// const NETHERMIND_ADDRESS = createAddress('0x61447385B019187daa48e91c55c02AF1F1f3F863');
+const NETHERMIND_ADDRESS = createAddress('0x5');
 const createFinding = (from: string, contract_address: string) => Finding.fromObject({
-  name: 'Kill Me function call Detected',
-  description: 'Kill Me function called on Curve-Stable-Swap contract.',
+  name: 'createAgent function call Detected',
+  description: 'createAgent function called on Nethermind Deployer contract.',
   alertId: ALERT_ID,
   severity: FindingSeverity.Low,
   type: FindingType.Suspicious,
   metadata: {
     from: from,
-    contract_address: contract_address,    
-},
+    contract_address: contract_address,
+  },
 });
 
-describe('Kill me agent for Curve StableSwap contract tests suite', () => {
-  let handleTransaction: HandleTransaction = provideCreateAgent(ALERT_ID, CONTRACT_ADDRESS);
-  let selector: string ;
+describe('createAgent called from Nethermind Deployer contract', () => {
+  let handleTransaction: HandleTransaction = provideCreateAgent(ALERT_ID, FORTA_ADDRESS);
+  let selector: string;
   let wrongSelector: string;
   beforeAll(() => {
-    selector = encodeFunctionSignature('kill_me()');
-    wrongSelector = encodeFunctionSignature('killme()');
+    selector = encodeFunctionSignature('createAgent()');
+    wrongSelector = encodeFunctionSignature('create_Agent()');
 
   });
 
-  it('should return kill_me function call finding', async () => {
+  it('should return createAgent function call finding', async () => {
 
     const txEvent: TransactionEvent = new TestTransactionEvent().addTraces({
-      from: SENDER_ADDRESS,
-      to: CONTRACT_ADDRESS,
+      from: NETHERMIND_ADDRESS,
+      to: FORTA_ADDRESS,
       input: selector,
     });
 
     const findings = await handleTransaction(txEvent);
-    expect(findings).toStrictEqual([createFinding(SENDER_ADDRESS, CONTRACT_ADDRESS)]);
+    expect(findings).toStrictEqual([createFinding(NETHERMIND_ADDRESS, FORTA_ADDRESS)]);
 
   });
 
   it('should ignore empty transactions', async () => {
     const tx: TransactionEvent = new TestTransactionEvent();
-    
+
     const findings = await handleTransaction(tx);
     expect(findings).toStrictEqual([]);
   });
@@ -65,19 +66,19 @@ describe('Kill me agent for Curve StableSwap contract tests suite', () => {
   it("should return empty finding because of wrong signature", async () => {
 
     const txEvent: TransactionEvent = new TestTransactionEvent().addTraces({
-      from: SENDER_ADDRESS,
-      to: CONTRACT_ADDRESS,
+      from: NETHERMIND_ADDRESS,
+      to: FORTA_ADDRESS,
       input: wrongSelector,
     });
 
     const findings = await handleTransaction(txEvent);
     expect(findings).toStrictEqual([]);
   });
-  
-  it("should ignore kill_me calls to other contracts", async () => {
+
+  it("should ignore createAgent calls to other contracts", async () => {
 
     const txEvent: TransactionEvent = new TestTransactionEvent().addTraces({
-      from: SENDER_ADDRESS,
+      from: NETHERMIND_ADDRESS,
       to: createAddress('0x2'),
       input: selector,
     });
@@ -85,4 +86,5 @@ describe('Kill me agent for Curve StableSwap contract tests suite', () => {
     const findings = await handleTransaction(txEvent);
     expect(findings).toStrictEqual([]);
   });
+
 });
--- agent/src/agent.ts
+++ agent/src/agent.ts
@@ -5,17 +5,17 @@ import {
   FindingType,
   ethers,
 } from 'forta-agent';
-import { 
-  FindingGenerator, 
+import {
+  FindingGenerator,
   provideFunctionCallsDetectorHandler,
 } from "forta-agent-tools";
 
-export const NETHERMIND_DEPLOYER_CONTRACT_ADDRESS = "0x61447385B019187daa48e91c55c02AF1F1f3F863"; 
+export const POLYGON_ADDRESS = "0x61447385B019187daa48e91c55c02AF1F1f3F863";
 const abi = ["function createAgent()"];
-export const iface = new ethers.utils.Interface(abi); 
+export const iface = new ethers.utils.Interface(abi);
 
-const createFindingGenerator = (alertId: string, address: string): FindingGenerator => 
-  (metadata: { [key: string]: any } | undefined): Finding => 
+const createFindingGenerator = (alertId: string, address: string): FindingGenerator =>
+  (metadata: { [key: string]: any } | undefined): Finding =>
     Finding.fromObject({
       name: 'createAgent function call Detected',
       description: 'createAgent function called on Nethermind Deployer contract.',
@@ -24,24 +24,24 @@ const createFindingGenerator = (alertId: string, address: string): FindingGenera
       type: FindingType.Suspicious,
       metadata: {
         from: metadata!.from,
-        contract_address: address,    
-    },
-  });
-  
+        contract_address: address,
+      },
+    });
+
 export const provideCreateAgent = (
   alertID: string,
   address: string
 ): HandleTransaction => provideFunctionCallsDetectorHandler(
-    createFindingGenerator(alertID, address), 
-    iface.getFunction('createAgent').format('sighash'), 
-    {
-      to: address,
-    }
+  createFindingGenerator(alertID, address),
+  iface.getFunction('createAgent').format('sighash'),
+  {
+    to: address,
+  }
 );
-  
+
 export default {
   handleTransaction: provideCreateAgent(
-    "ALERT-0", 
-    NETHERMIND_DEPLOYER_CONTRACT_ADDRESS,
+    "ALERT-0",
+    POLYGON_ADDRESS,
   )
 };
